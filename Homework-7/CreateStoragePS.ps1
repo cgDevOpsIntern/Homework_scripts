f9526baeb400d59fb7cdbc96c78e338259eae019
pav <pawel.aleksandruk@capgemini.com>
2022-08-25 01:54:25 +0200

    updated createStorage.sh script test01

--- Homework-7/CreateStoragePS.ps1
+++
@@ -1,5 +0,0 @@
-$rg = "amdemo"
-
-foreach ($i in 1..10) {
-    
-}
--- Homework-7/createStorage.sh
+++ Homework-7/createStorage.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+echo "Creating new Account Storage"
 # Create an Azure storage account in the resource group.
 az storage account create \
   --name Storage-Account \
   --location uksouth \
   --resource-group build-agents-01 \
   --sku Standard_LRS \
-  --vnet-name build-agents-01-vnet \
-  --subnet agents-subnet
+  #--vnet-name build-agents-01-vnet \
+  #--subnet agents-subnet 
+
+echo "New Account Storage created"
+
+az storage account list -g build-agents-01

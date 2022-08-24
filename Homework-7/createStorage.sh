31d125747b6400c6fa1f5b3fc7675149f2325734
pav <pawel.aleksandruk@capgemini.com>
2022-08-25 01:03:55 +0200

    add createStorage.sh script

---
+++ Homework-7/createStorage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create an Azure storage account in the resource group.
+az storage account create \
+  --name Storage-Account \
+  --location uksouth \
+  --resource-group build-agents-01 \
+  --sku Standard_LRS \
+  --vnet-name build-agents-01-vnet \
+  --subnet agents-subnet
+
+Clean up deployment

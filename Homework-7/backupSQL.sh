4ebc5d3c181f10bbc613f4de6014b2203ae0519b
pav <pawel.aleksandruk@capgemini.com>
2022-08-26 13:06:20 +0200

    backupSQL.sh script updated -test01

--- Homework-7/backupSQL.sh
+++ Homework-7/backupSQL.sh
@@ -20,16 +20,76 @@ storage="storage01sah7$randomIdentifier"
 container="agents-backup02-azuresql-container-$randomIdentifier"
 bacpac="backup.bacpac"
 
-echo "Creating new Account Storage"
+# Previous script for creating Storage -not tested -START
+#echo "Creating new Account Storage"
 # Create an Azure storage account in the resource group.
-az storage account create \
-  --name $storage \
-  --location $location \
-  --resource-group $resourceGroup \
-  --sku Standard_LRS \
+#az storage account create \
+#  --name $storage \
+#  --location $location \
+#  --resource-group $resourceGroup \
+#  --sku Standard_LRS \
   #--vnet-name build-agents-01-vnet \
   #--subnet agents-subnet 
 
-echo "New Account Storage created"
+#echo "New Account Storage created"
 
-az storage account list -g build-agents-01
+#az storage account list -g build-agents-01
+
+# Previous script for creating Storage -not tested -STOP
+
+######
+echo "Using resource group $resourceGroup with login: $login, password: $password..."
+
+echo "Creating $resourceGroup in $location..."
+az group create 
+--name $resourceGroup \
+--location $location \
+--tags $tag
+
+echo "Creating $storage..."
+az storage account create 
+--name $storage \
+--resource-group $resourceGroup \
+--location $location \ 
+--sku $sku
+
+echo "Creating $container on $storage..."
+key=$(az storage account keys list --account-name $storage --resource-group $resourceGroup -o json --query [0].value | tr -d '"')
+az storage container create 
+--name $container \
+--account-key $key \
+--account-name $storage
+
+echo "Creating $server in $location..."
+az sql server create 
+--name $server \
+--resource-group $resourceGroup \
+--location $location \
+--admin-user $login \
+--admin-password $password
+
+az sql server firewall-rule create 
+--resource-group $resourceGroup \
+--server $server \
+--name AllowAzureServices \
+--start-ip-address 0.0.0.0 \
+--end-ip-address 0.0.0.0
+
+echo "Creating $database..."
+az sql db create 
+--name $database \
+--resource-group $resourceGroup \
+--server $server \
+--edition GeneralPurpose \
+--sample-name SampleDBh7
+
+echo "Backing up $database..."
+az sql db export 
+--admin-password $password \ 
+--admin-user $login \ 
+--storage-key $key \
+--storage-key-type StorageAccessKey \
+--storage-uri "https://$storage.blob.core.windows.net/$container/$bacpac" \
+--name $database \
+--resource-group $resourceGroup \
+--server $server

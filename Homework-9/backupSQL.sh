abae1088b89fe1511bde60902c5fab02d12a6e3f
pav <pawel.aleksandruk@capgemini.com>
2022-08-27 15:27:44 +0200

    renamed Homework-9 folder and added Homework-7 folder

--- Homework-9/backupSQL.sh
+++ Homework-9/backupSQL.sh
@@ -35,19 +35,22 @@ bacpac="backup.bacpac"
 ######
 echo "Using resource group $resourceGroup with login: $login, password: $password..."
 
+#Creating a resource group
 echo "Creating $resourceGroup in $location..."
 az group create 
 --name $resourceGroup \
---location "$location" \
---tags $tag
+--location $location \
+--tags $tag\
 
+#Creating a storage
 echo "Creating $storage..."
 az storage account create 
 --name $storage \
 --resource-group $resourceGroup \
---location "$location" \ 
+--location $location \ 
 --sku $sku
 
+#Creating a contianer on storage
 echo "Creating $container on $storage..."
 key=$(az storage account keys list --account-name $storage --resource-group $resourceGroup -o json --query [0].value | tr -d '"')
 az storage container create 
@@ -55,11 +58,12 @@ az storage container create
 --account-key $key \
 --account-name $storage \
 
+# Creating a server and firewall-rule
 echo "Creating $server in $location..."
 az sql server create 
 --name $server \
 --resource-group $resourceGroup \
---location "$location" \
+--location $location \
 --admin-user $login \
 --admin-password $password \
 
@@ -70,6 +74,7 @@ az sql server firewall-rule create
 --start-ip-address 0.0.0.0 \
 --end-ip-address 0.0.0.0 \
 
+# Create DB
 echo "Creating $database..."
 az sql db create 
 --name $database \
@@ -78,13 +83,14 @@ az sql db create
 --edition GeneralPurpose \
 --sample-name SampleDBh7 \
 
-echo "Backing up $database..."
-az sql db export 
---admin-password $password \ 
---admin-user $login \ 
---storage-key $key \
---storage-key-type StorageAccessKey \
---storage-uri "https://$storage.blob.core.windows.net/$container/$bacpac" \
---name $database \
---resource-group $resourceGroup \
---server $server \
+# Backup DB
+#echo "Backing up $database..."
+#az sql db export 
+#--admin-password $password \ 
+#--admin-user $login \ 
+#--storage-key $key \
+#--storage-key-type StorageAccessKey \
+#--storage-uri "https://$storage.blob.core.windows.net/$container/$bacpac" \
+#--name $database \
+#--resource-group $resourceGroup \
+#--server $server \

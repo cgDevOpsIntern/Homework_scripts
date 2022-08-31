29e28ce9690de158ddab06635c7ead4a83af31ac
pav <pawel.aleksandruk@capgemini.com>
2022-08-31 10:31:42 +0200

    added Homework-8 folder and starting app-service.sh file

---
+++ Homework-8/app-service.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# 1. to see variables ad "-x" to "hashbang/bin/bash" line on top.
+# 2. Script that can create Linux based App Service for 
+#    Input parameters: 
+#   -Region -Name -Pricing tier -Nodes in web farm -Docker Hub image to be used
+#
+
+# Variable block
+resourceGroup="homework7-build-grup"
+location="westeurope"

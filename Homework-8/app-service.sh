b419d1c92b9a7aa10378c0c1ed918b9126348147
Pav <pavcapgem@gmail.com>
2022-08-31 14:36:12 +0200

    app-service.sh update 01

--- Homework-8/app-service.sh
+++ Homework-8/app-service.sh
@@ -5,8 +5,45 @@
 # 2. Script that can create Linux based App Service for 
 #    Input parameters: 
 #   -Region -Name -Pricing tier -Nodes in web farm -Docker Hub image to be used
+# 3. Changed "resourceGroup" variable name to "rGgroup"
 #
 
 # Variable block
-resourceGroup="homework7-build-grup"
+# for the plan
+rGroup="homework7-build-grup"
 location="westeurope"
+sPlan="linux-app-service-plan"
+workers="1"
+sku="B1"
+#for an app
+appName="homework8app"
+containerImg="mcr.microsoft.com/dotnet/samples:aspnetapp"
+
+# Creating new VM
+echo "...>>>..."
+echo ">>>>Creating new Plan $sPlan<<<<"
+echo "...>>>..."
+
+az appservice plan create \
+    --resource-group "$rGroup" \
+    --name "$sPlan" \
+    --location "$location" \
+    --is-linux \
+    --number-of-workers "$workers" \ 
+    --sku "$SKU" \
+
+# Creating new APP
+echo "...>>>..."
+echo ">>>>Creating APP $appName<<<<"
+echo "...>>>..."
+
+az webapp create\
+    --resource-group "$rGroup" \
+    --plan "$sPlan" \
+    --name "$appName" \
+    --deployment-container-image-name "$containerImg" \
+
+# New Service Plan and new App created
+echo "...>>>..."
+echo ">>>>New Service Plan and new App on $resourceGroup created<<<<"
+echo "...>>>..."
